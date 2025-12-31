579293e3ccc41c2e186576c3de3ba3624dcb1af7
archanachoudhri-del <archana.choudhri@rungta.org>
2025-12-31 20:41:55 +0530

    Delete Practical 4 directory

--- Practical 4/missing.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-    printf("ARCHNA CHOUDHRI\t10266\n");
-    printf("hollo")
-        return 0;
-}
-//missing.c:5:9: error: expected ';' before 'return'
-//  return 0;
-        //  ^~~~~~
